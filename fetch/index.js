e1d067919408e2ed425bb080dd10dba932374b6d
deividgp <dayd4@outlook.com>
2022-11-02 19:24:43 +0100

    index modified

--- fetch/index.js
+++ fetch/index.js
@@ -3,47 +3,44 @@ import twilio from "twilio";
 dotenv.config();
 
 const twilioClient = twilio(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN);
+const timeout = 5000;
 let firstPage = null;
 let different = false;
 
-async function main()
-{
-    await recursive();
-}
-
-async function recursive()
-{
+async function main() {
     fetch(process.env.ENDPOINT)
         .then(response => response.json())
         .then(data => {
-            if(data == null){
-                setTimeout(recursive, 5000);
+            if (data == null) {
+                setTimeout(recursive, timeout);
                 return;
             }
 
             const splitData = data.split("\n");
-            splitData.splice(11,1);
+            splitData.splice(11, 1);
             const newPage = splitData.join("\n");
 
-            if(firstPage == null){
+            if (firstPage == null) {
                 firstPage = newPage;
-            }else{
-                if (newPage == firstPage){
-                    console.log("SAME");
-                } else if(firstPage != newPage && !different){
-                    console.log("DIFFERENT");
-                    twilioClient.messages
+                setTimeout(recursive, timeout);
+                return;
+            }
+
+            if (newPage == firstPage) {
+                console.log("SAME");
+            } else if (firstPage != newPage && !different) {
+                console.log("DIFFERENT");
+                twilioClient.messages
                     .create({
                         body: "TICKET AVAILABLE",
                         messagingServiceSid: process.env.TWILIO_MESSAGINGSID,
                         to: process.env.TWILIO_TO
                     })
                     .done();
-                    different = true;
-                }
+                different = true;
             }
 
-            setTimeout(recursive, 5000);
+            setTimeout(main, timeout);
         })
 }
 
